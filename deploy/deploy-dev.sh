1acb924a8241e285bfb9daaba2b88b21df28e200
Mustafa Taleb <mustafa@mustafataleb.com>
2017-09-27 03:06:28 -0500

    added travis specific compose files

--- deploy/deploy-dev.sh
+++ deploy/deploy-dev.sh
@@ -9,10 +9,9 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 
-gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME}
-gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME}
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME} gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME}:latest
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME} gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME}:latest
+gcloud docker -- push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME}
+gcloud docker -- push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME}
 kubectl config view
 kubectl config current-context
-kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_REACT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME}:latest ${KUBE_DEPLOYMENT_FLASK_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME}:latest 
+kubectl set image deployment/${KUBE_DEPLOYMENT_REACT_CONTAINER_NAME} ${KUBE_DEPLOYMENT_REACT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME}
+kubectl set image deployment/${KUBE_DEPLOYMENT_FLASK_CONTAINER_NAME} ${KUBE_DEPLOYMENT_FLASK_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME}:latest 

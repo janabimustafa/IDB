2b8aac1095062f8fbe2c7231fe8d2d1a2e716e4c
Mustafa Taleb <mustafa@mustafataleb.com>
2017-09-26 23:39:59 -0500

    Added travis integration with GCP (still testing)

---
+++ deploy/deploy-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud --quiet config set project $PROJECT_NAME_PRD
+gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
+gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
+
+gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME}
+gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME}
+yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME} gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME}:latest
+yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME} gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME}:latest
+kubectl config view
+kubectl config current-context
+kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_REACT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_REACT_IMAGE_NAME}:latest ${KUBE_DEPLOYMENT_FLASK_CONTAINER_NAME}=gcr.io/${PROJECT_NAME_PRD}/${DOCKER_FLASK_IMAGE_NAME}:latest 

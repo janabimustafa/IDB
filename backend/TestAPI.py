afa3ad2b516f8f056ac2b8eca85f0b389074c43c
John O'kane <jtommyok8@gmail.com>
2017-10-26 14:56:13 -0500

    remaining tests written

--- backend/TestAPI.py
+++ backend/TestAPI.py
@@ -43,7 +43,7 @@ class TestRLDBAPI(TestCase):
 		self.assertEqual(isinstance(obj, Body))
 
 	def testGetBodyById(self):
-		result = urlopen("http://dev.rldb.me/api/bodies/0").read().decode()
+		result = urlopen("http://dev.rldb.me/api/bodies/7071").read().decode()
 		obj = desserialize(result)
 		self.assertEqual(isinstance(obj, Body))
 
@@ -63,6 +63,58 @@ class TestRLDBAPI(TestCase):
 		result = urlopen("http://dev.rldb.me/api/decals/0").read().decode()
 		obj = desserialize(result)
 		self.assertEqual(isinstance(obj, Decal))
+
+	def testGetAllCrates(self):
+		result = urlopen("http://dev.rldb.me/api/crates").read().decode()
+		obj_list = deserialize_list(result)
+		self.assertTrue(len(obj) > 1)
+		for obj in object_list:
+			self.assertTrue(isinstance(obj, Crate))
+
+	def testGetCrateByName(self):
+		result = urlopen("http://dev.rldb.me/api/Crate/Turbo%20Crate").read().decode()
+		obj = desserialize(result)
+		self.assertEqual(isinstance(obj, Crate))
+
+	def testGetCrateByID(self):
+		result = urlopen("http://dev.rldb.me/api/Crate/6991").read().decode()
+		obj = desserialize(result)
+		self.assertEqual(isinstance(obj, Crate))
+
+	def testGetAllDLC(self):
+		result = urlopen("http://dev.rldb.me/api/dlcs").read().decode()
+		obj_list = deserialize_list(result)
+		self.assertTrue(len(obj) > 1)
+		for obj in object_list:
+			self.assertTrue(isinstance(obj, DLC))
+
+	def testGetDLCByName(self):
+		result = urlopen(r"http://dev.rldb.me/api/dlcs/Supersonic%20Fury").read().decode()
+		obj = desserialize(result)
+		self.assertEqual(isinstance(obj, DLC))
+
+	def testGetDLCByID(self):
+		result = urlopen(r"http://dev.rldb.me/api/dlcs/0").read().decode()
+		obj = desserialize(result)
+		self.assertEqual(isinstance(obj, DLC))
+
+	def testGetObjectByID(self):
+		result = urlopen("http://dev.rldb.me/api/id/6991").read().decode()
+		obj = desserialize(result)
+		self.assertEqual(isinstance(obj, RLObject))
+		self.assertEqual(isinstance(obj, Crate))
+
+	def testGetAllPlayers(self):
+		result = urlopen("http://dev.rldb.me/api/players").read().decode()
+		obj_list = deserialize_list(result)
+		self.assertTrue(len(obj) > 1)
+		for obj in object_list:
+			self.assertTrue(isinstance(obj, Player))
+
+	def testGetPlayerByID(self):
+		result = urlopen(r"http://dev.rldb.me/api/players/76561198067659330").read().decode()
+		obj = desserialize(result)
+		self.assertEqual(isinstance(obj, Player))
 
 if __name__ == "__main__":
     main()

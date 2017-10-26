9e70dc5323dbdd552b129aceed2268611ccae665
Melanie Rivera <obits13@outlook.com>
2017-10-26 14:53:54 -0500

    updated player card with attributes

--- frontend/rldb/src/PlayerCard.js
+++ frontend/rldb/src/PlayerCard.js
@@ -4,15 +4,22 @@ import './playerCard.css';
 
 class PlayerCard extends Component {
 
+    getPlatform (platform) {
+        var platforms = ["unknown", "Steam", "Playstation", "Xbox"];
+        return platforms[platform];
+    } 
+
     render() {
         return (
-            <div className=" col-md-4 col-sm-6 text-center">
+            <div className=" col-md-3 col-sm-3 text-center">
                 <a href="">
-                    <img className="img-rounded" src={this.props.data.image}/>
+                    <img className="img-rounded" src={this.props.data.image ? this.props.data.image : "http://via.placeholder.com/150x150"}/>
                     <h2>{this.props.data.name}</h2>
                 </a>
                 <div className="caption">
-                    <p>Platform: {this.props.data.platform ? this.props.data.platform : "unknown"}</p>
+                    <p>Platform: {this.getPlatform(this.props.data.platform)}</p>
+                    <p>Skill Rating: {this.props.data.skill_rating}</p>
+                    <p>Wins: {this.props.data.wins}</p>
                 </div>
             </div>
         )

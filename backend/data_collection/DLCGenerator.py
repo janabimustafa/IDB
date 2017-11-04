c25642ff409d72c1d73769becf4bd791d84ebafb
Mustafa Taleb <mustafa@mustafataleb.com>
2017-11-03 23:23:25 -0500

    added dlc descriptions

--- backend/data_collection/DLCGenerator.py
+++ backend/data_collection/DLCGenerator.py
@@ -18,6 +18,7 @@ dlcs = [
         'release_date': '2015-8-13',
         'image': 'http://www.dlcompare.com/upload/gameimage/file/7522.jpeg',
         'type': 'dlc',
+        'description': 'Supersonic Fury was the first major DLC pack to be released for Rocket League.',
         'items': [
             get_hash('body' + 'Dominus'),
             get_hash('body' + 'Takumi'),
@@ -50,6 +51,7 @@ dlcs = [
         'release_date': '2015-10-13',
         'image': 'http://ecx.images-amazon.com/images/I/61OeESrZ35L._SX342_QL70_.jpg',
         'type': 'dlc',
+        'description': 'Revenge of the Battle-Cars was the second major DLC pack to be released for Rocket League.',
         'items': [
             get_hash('body' + 'Scarab'),
             get_hash('body' + 'Zippy'),
@@ -85,6 +87,7 @@ dlcs = [
         'id': get_hash('dlc' + 'Chaos Run'),
         'name': 'Chaos Run',
         'release_date': '2015-12-1',
+        'description': 'Chaos Run was the third major DLC pack added to the game.',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/0/03/Chaosr.jpg/revision/latest?cb=20161110054220',
         'type': 'dlc',
         'items': [
@@ -119,6 +122,7 @@ dlcs = [
     {
         'id': get_hash('dlc' + 'DeLorean Time Machine'),
         'release_date': '2015-10-21',
+        'description': 'Great Scott! The time continuum has been disrupted and as of October 21, 2015, the "Future" is now!\nCelebrate one of the most anticipated days of the last 30 years in Rocket League with the official Back to the Future DeLorean Time Machine!\nDon\'t waste another minute. Get yours today!',
         'name': 'DeLorean Time Machine',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/9/95/DeLorean_Time_Machine_promo_art_1.jpg/revision/latest/scale-to-width-down/310?cb=20170530114548',
         'type': 'dlc',
@@ -126,6 +130,7 @@ dlcs = [
     },
     {
         'id': get_hash('dlc' + 'Batman v Superman: Dawn of Justice'),
+        'description': 'Black and blue, god vs man, day vs night!\nUnleash your own one-car reign of terror with the official Batmobile Battle-Car from the upcoming blockbuster mega-film, Batman v Superman: Dawn of Justice!\nThree exclusive antenna flags to pledge your allegiance to the World\'s Finest trio of Batman, Superman, or Wonder Woman.',
         'release_date': '2016-3-8',
         'name': 'Batman v Superman: Dawn of Justice',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/3/3c/Batman_v_Superman_Dawn_of_Justice_DLC_pack.jpg/revision/latest/scale-to-width-down/310?cb=20170710214612',
@@ -135,6 +140,7 @@ dlcs = [
     {
         'id': get_hash('dlc' + 'Aftershock'),
         'name': 'Aftershock',
+        'description': 'Drive the fan-favorite, jet-inspired dynamo, Aftershock, and relive the glory days of the original Supersonic Acrobatic Rocket-Powered Battle-Cars, today!',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/2/2e/Aftershock_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170531180331',
         'type': 'dlc',
         'release_date': '2016-7-18',
@@ -152,6 +158,7 @@ dlcs = [
     {
         'id': get_hash('dlc' + 'Marauder'),
         'name': 'Marauder',
+        'description': 'Because you demanded it, a Supersonic Acrobatic Rocket-Powered Battle-Cars classic, the monstrous Marauder, returns to the pitch!',
         'release_date': '2016-7-18',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/7/79/Marauder_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170531202512',
         'type': 'dlc',
@@ -169,6 +176,7 @@ dlcs = [
     {
         'id': get_hash('dlc' + 'Esper'),
         'name': 'Esper',
+        'description': 'Taken from the streets of Japan\'s not-too-distant future, Esper combines roadster and motorcycle for the coolest ride in the apocalypse!',
         'release_date': '2016-7-18',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/3/3b/Esper_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170601160548',
         'type': 'dlc',
@@ -187,6 +195,7 @@ dlcs = [
         'id': get_hash('dlc' + 'Masamune'),
         'name': 'Masamune',
         'release_date': '2016-7-18',
+        'description': 'Use the science of aerodynamics and slice through the competition with Masamune, a cutting-edge racer from the streets of Neo Tokyo!',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/8/81/Masamune_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170601162859',
         'type': 'dlc',
         'items': [
@@ -203,6 +212,7 @@ dlcs = [
     {
         'id': get_hash('dlc' + 'Proteus'),
         'name': 'Proteus',
+        'description': 'From the darkest fathoms of the ocean comes Proteus, an imposing deep-sea diver built to handle pressure!',
         'release_date': '2016-10-4',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/c/cf/Proteus_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170602180338',
         'type': 'dlc',
@@ -221,6 +231,7 @@ dlcs = [
         'id': get_hash('dlc' + 'Triton'),
         'name': 'Triton',
         'release_date': '2016-10-4',
+        'description': 'Embrace your inner sea god and rule the high seas with Triton, the AquaDome\'s sleekest submersible Battle-Car!',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/9/94/Triton_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170602185705',
         'type': 'dlc',
         'items': [
@@ -238,6 +249,7 @@ dlcs = [
         'id': get_hash('dlc' + 'Vulcan'),
         'name': 'Vulcan',
         'release_date': '2016-12-6',
+        'description': 'Defend the universe (and your goal line) with Vulcan, the most elite sports-craft in the galaxy!',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/6/64/Vulcan_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170529185045',
         'type': 'dlc',
         'items': [
@@ -255,6 +267,7 @@ dlcs = [
         'id': get_hash('dlc' + 'TWIN MILL III'),
         'name': 'Twin Mill III',
         'release_date': '2017-02-21',
+        'description': 'Powered by not one, but TWO blown big-block engines and dual front intakes, Hot Wheels® world famous Twin Mill™ III has arrived!',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/3/30/Twin_Mill_III_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170611140210',
         'type': 'dlc',
         'items': [
@@ -271,6 +284,7 @@ dlcs = [
     {
         'id': get_hash('dlc' + 'Bone Shaker'),
         'name': 'Bone Shaker',
+        'description': 'Take control of the field with one of the most beloved hot rods of all time, the Hot Wheels® Bone Shaker™!',
         'release_date': '2017-02-21',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/4/41/Bone_Shaker_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170611123904',
         'type': 'dlc',
@@ -289,6 +303,7 @@ dlcs = [
         'id': get_hash('dlc' + 'Ice Charger'),
         'name': 'Ice Charger',
         'release_date': '2017-04-04',
+        'description': 'Now you can own Dom Toretto\'s Dodge Ice Charger from Universal Pictures\' explosive movie, The Fate of the Furious.',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/6/68/Ice_Charger_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20170530130044',
         'type': 'dlc',
         'items': [
@@ -305,6 +320,7 @@ dlcs = [
         'id': get_hash('dlc' + '\'70 DODGE CHARGER R/T'),
         'name': '\'70 Dodge Charger R/T',
         'release_date': '2017-10-11',
+        'description': 'Challenge the rules with Dom Toretto\'s 1970 Dodge Charger R/T from from the film that launched the epic saga, The Fast and the Furious, by Universal Pictures.',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/4/49/%2770_Dodge_Charger_RT_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20171004211508',
         'type': 'dlc',
         'items': [
@@ -322,6 +338,7 @@ dlcs = [
         'id': get_hash('dlc' + '\'99 Nissan Skyline GT-R R34'),
         'name': '\'99 Nissan Skyline GT-R R34',
         'release_date': '2017-10-11',
+        'description': 'Rev up Brian O\'Conner\'s 1999 Nissan Skyline GT-R R34 from Universal Pictures\' 2 Fast 2 Furious!',
         'image': 'https://vignette.wikia.nocookie.net/rocketleague/images/a/a5/%2799_Nissan_Skyline_GT-R_R34_hero_art.jpg/revision/latest/scale-to-width-down/310?cb=20171004211431',
         'type': 'dlc',
         'items': [

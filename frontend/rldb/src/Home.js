1badf0e6cde50326c18ab72ca25b129acfa3447f
Melanie Rivera <obits13@outlook.com>
2017-11-09 12:00:47 -0600

    Added cover photo to repo, fixed Home text

--- frontend/rldb/src/Home.js
+++ frontend/rldb/src/Home.js
@@ -4,10 +4,10 @@ import './Home.css'
 
 const Home = () => (
     <div className="cover-img">
-        <img className="splash" src="http://fullhdpictures.com/wp-content/uploads/2016/01/Wonderful-Rocket-League-Wallpaper.jpg" alt="Futuristic soccer ball in a stadium"/>
-        <div className="col-md-6 col-sm-6">
+        <img className="splash" src="/Wonderful-Rocket-League-Wallpaper.jpg" alt="Futuristic soccer ball in a stadium"/>
+        <div className="col-md-5 col-sm-5 col-lg-6">
         </div>
-        <div className="col-md-6 col-sm-6 text-container">
+        <div className="col-md-6 col-sm-6 col-lg-5 text-container">
             <h1 className="light-text">Rocket League Database</h1>
             <p className="light-text">on RLDB.me</p>
         </div>

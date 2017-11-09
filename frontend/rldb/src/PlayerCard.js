ba04d0799b5e93ffeba4ec505dde9926f2040f78
Mustafa Taleb <mustafa@mustafataleb.com>
2017-11-09 14:50:12 -0600

    Fixed players 404

--- frontend/rldb/src/PlayerCard.js
+++ frontend/rldb/src/PlayerCard.js
@@ -9,11 +9,18 @@ class PlayerCard extends Component {
         var platforms = ["unknown", "Steam", "Playstation", "Xbox"];
         return platforms[platform];
     } 
-
+    makePlural (noun) {
+        var lastChar = noun.slice(-1)
+        console.log('last char in ' + noun + ' is ' + lastChar);
+        if (lastChar === 'y')
+            return noun.replace('y', 'ies');
+        else
+            return noun + 's';
+    }
     render() {
         return (
             <div className="player-card grow col-md-2 col-sm-4 text-center">
-                <Link to={`/${this.props.data.type}/${this.props.data.name}`}>
+                <Link to={`/${this.makePlural(this.props.data.type)}/${this.props.data.name}`}>
                     <img className="img-rounded img-player" src={this.props.data.image ? this.props.data.image : "http://via.placeholder.com/150x150"}  alt="player"/>
                     <h2>{this.props.data.name}</h2>
                 </Link>
